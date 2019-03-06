a7d1da13591e84f48dfd39eebf4e2e50f0e2f413
picolloo <lucaspicolloo@gmail.com>
2019-03-06 20:54:20 -0300

    backend implemented

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const path = require("path");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+io.on("connection", function(socket) {
+  socket.on("chat_msg", function(msg) {
+    io.emit("chat_msg", msg);
+  });
+
+  socket.on("disconnect", function() {
+    // console.log("user disconnected");
+  });
+});
+
+http.listen(8080, () => "server bound to port 8080");
