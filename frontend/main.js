efd647d8e723d78749023a5a0f7807a5a543774e
DEV\lucas.picollo <lucas.picollo@audaces.com>
2019-03-07 13:29:21 -0300

    vue included and app injected in index

---
+++ frontend/main.js
@@ -0,0 +1,11 @@
+import { Vue } from 'vue'
+import { App } from "./components/App"
+
+Vue.config.procutionTip = false;
+
+
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+})
