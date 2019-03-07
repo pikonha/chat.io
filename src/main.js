c87f47ca5a33664a72be1c20b113a2775f883763
DEV\lucas.picollo <lucas.picollo@audaces.com>
2019-03-07 17:23:27 -0300

    components being served by express

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { Vue } from 'vue'
-import { App } from "./components/App"
+import Vue from 'vue'
+import App from "./App.vue"
 
 Vue.config.procutionTip = false;
 
 new Vue({
-  el: "#app",
-  components: { App },
-  template: "<App/>"
-})
+  render: h => h(App),
+}).$mount('#app')
