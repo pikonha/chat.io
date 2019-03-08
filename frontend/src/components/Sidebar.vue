9e022bce9811deef96bf33e2c0adff0e2916f81d
picolloo <lucaspicolloo@gmail.com>
2019-03-07 22:38:18 -0300

    project must be detached in front and backend

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="sidebar-chat">
+    <img id="user-icon" src="../assets/user.png" alt="User Icon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarChat",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+#sidebar-chat {
+  grid-area: sidebar;
+  background: #775144;
+}
+
+#user-icon {
+  max-width: 70%;
+  border-radius: 100px;
+  margin: 20px;
+}
+</style>
