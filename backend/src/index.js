31e401e822093ce59ecc8e9686e67e789772ddbb
picolloo <lucaspicolloo@gmail.com>
2019-03-08 21:36:48 -0300

    frontend communicating with server by sockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,23 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
 const morgan = require("morgan");
+const io = require("socket.io")(http);
 
 app.use(morgan("dev"));
 
 io.on("connection", function(socket) {
   socket.on("chat_msg", function(msg) {
-    io.emit("chat_msg", msg);
+    console.log("Msg receive:", msg);
   });
-
   socket.on("disconnect", function() {});
+
+  socket.emit("hi", "everyone");
+});
+
+io.of("/room").on("connection", function(socket) {
+  socket.emit("room_msg", "hi room");
+
+  // socket.on("greet", msg => console.log(msg));
 });
 
 http.listen(8080);
